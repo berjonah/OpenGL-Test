91316c9852c27f71f2484c6163095df2f40e6393
Andrew Fillmore <fillmoreandrew110@gmail.com>
2015-05-28 15:06:29 -0600

    Added Main

---
+++ src/Program.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Andrew Fillmore
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
